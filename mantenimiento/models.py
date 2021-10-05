14beba433ec04657fbc3f9bfea5bce0e2e7ca4cc
tuflesa <tecnico@tuflesa.es>
2021-10-05 18:10:51 +0200

    cambio en los modelos de la app de mantenimiento

--- mantenimiento/models.py
+++ mantenimiento/models.py
@@ -3,6 +3,7 @@ from django.db.models.base import Model
 from django.db.models.deletion import SET_NULL
 from estructura.models import Equipo
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Notificacion(models.Model): # Notificación 5W+2H Plus
     # 5W+2H
@@ -14,7 +15,23 @@ class Notificacion(models.Model): # Notificación 5W+2H Plus
     cuanto = models.TextField(max_length=150) # How many. Cuantas veces ocurre el problema: Una vez al día, continuamente, ...
     porque = models.TextField(max_length=250) # Why. Por que cree que ocurre el problema.
     # Plus
+    fecha_creacion = models.DateField(default=timezone.now)
+    # para = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # A quien se informa del problema
+    descartado = models.BooleanField(default=False)
+    conclusion = models.TextField(max_length=250, blank=True, null=True) # Explicación de si queda resuelto o motivos por los que se descarta
 
+    def abierto(self):
+        cerrado = False
+        if self.descartado:
+            return cerrado
+        # Si todos los partes de esta notificación están cerrados False sino True
+        for parte in self.partes:
+            if not parte.finalizado:
+                break
+        return not cerrado
+        
+    def __str__(self):
+        return self.que
 
 class Especialidad(models.Model): # Electricidad, Electronica, Mecanica, Fontanería ...
     nombre = models.CharField(max_length=50)
@@ -50,17 +67,35 @@ class Tarea(models.Model):
 
 class ParteTrabajo(models.Model):
     nombre = models.CharField(max_length=150)
-    notificacion = models.ForeignKey(Notificacion, on_delete=models.CASCADE, null=True, blank=True)
+    notificacion = models.ForeignKey(Notificacion, on_delete=models.CASCADE, null=True, blank=True, related_name='partes')
     tipo = models.ForeignKey(TipoTarea, on_delete=models.CASCADE)
     creada_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='partes_creados')
     responsable = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='partes_responsable')
-    finalizado = models.BooleanField(default=False)
+    # finalizado = models.BooleanField(default=False)
     observaciones = models.TextField()
+
+    def finalizado(self):
+        fin = True
+        for linea in self.lineas:
+            if linea.fecha_fin is None:
+                fin = False
+                break
+        return fin
+
+    def fecha_fin(self):
+        fecha = None
+        if not self.finalizado:
+            return fecha
+        for linea in self.lineas:
+            if fecha < linea.fecha_fin:
+                fecha = linea.fecha_fin
+        return fecha
 
     def __str__(self):
         return self.nombre
 
 class LineaParteTrabajo(models.Model):
+    parte = models.ForeignKey(ParteTrabajo, on_delete=models.CASCADE, related_name='lineas')
     tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
     fecha_inicio = models.DateField(blank=True, null=True)
     fecha_fin = models.DateField(blank=True, null=True)
