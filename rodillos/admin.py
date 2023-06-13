9e063b3bb27ed5e1df87cdcfab10ab5efed7a2b1
tuflesa <tecnico@tuflesa.es>
2023-06-13 12:08:04 +0200

    migraciones

--- rodillos/admin.py
+++ rodillos/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Seccion, Operacion, Posicion
+from .models import Seccion, Operacion, Posicion, Rodillo, Conjunto, Elemento, Ficheros, Material, Montaje, Grupo
 
 admin.site.register(Seccion)
 admin.site.register(Operacion)
 admin.site.register(Posicion)
+admin.site.register(Rodillo)
+admin.site.register(Conjunto)
+admin.site.register(Elemento)
+admin.site.register(Ficheros)
+admin.site.register(Material)
+admin.site.register(Montaje)
+admin.site.register(Grupo)
