265fa86e16f24b479c829ff82a24b4afc4a81283
tuflesa <tecnico@tuflesa.es>
2023-06-30 11:42:30 +0200

    Crear Rodillo Serializer y ViewSet

--- rodillos/serializers.py
+++ rodillos/serializers.py
@@ -1,17 +1,9 @@
 from rest_framework import serializers
-from rodillos.models import Seccion, Operacion, Posicion, Conjunto, Elemento, Rodillo, Montaje, Grupo, Ficheros, Material
-from administracion.serializers import UserSerializer
-from estructura.serializers import EquipoSerializer, SeccionSerializer, ZonaSerializer
+from rodillos.models import Rodillo
 
-class ConjuntoSerializer(serializers.ModelSerializer):
+class RodilloSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Conjunto
-        fields = ['id', 'nombre', 'elementos', 'tubo_madre']
-
-class GruposSerializer(serializers.ModelSerializer):
-    conjuntos = ConjuntoSerializer(many=True, read_only=True)
-    class Meta:
-        model = Grupo
-        fields = ['id', 'nombre', 'conjuntos', 'tubo_madre', 'maquina']
+        model = Rodillo
+        fields = ['id', 'nombre', 'operacion', 'grupo', 'tipo', 'material']
 
 
--- rodillos/views.py
+++ rodillos/views.py
@@ -1,31 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rodillos.models import Seccion, Operacion, Posicion, Conjunto, Elemento, Rodillo, Montaje, Grupo, Material, Ficheros
-from rodillos.serializers import ConjuntoSerializer, GruposSerializer
+from rodillos.models import Rodillo, Tipo_rodillo, Seccion, Operacion, Eje
+from rodillos.serializers import RodilloSerializer
 from django_filters import rest_framework as filters
-from rest_framework.pagination import PageNumberPagination
 
-class GrupoFilter(filters.FilterSet):
+class RodilloFilter(filters.FilterSet):
     class Meta:
-        model = Grupo
+        model = Rodillo
         fields = {
             'nombre': ['exact'],
         }
 
-class ConjuntoFilter(filters.FilterSet):
-    class Meta:
-        model = Conjunto
-        fields = {
-            'nombre': ['exact'],
-        }
+class RodilloViewSet(viewsets.ModelViewSet):
+    serializer_class = RodilloSerializer
+    queryset = Rodillo.objects.all()
+    filterset_class = RodilloFilter
 
-class ConjuntoViewSet(viewsets.ModelViewSet):
-    serializer_class = ConjuntoSerializer
-    queryset = Conjunto.objects.all()
-    filterset_class = ConjuntoFilter
-    
-class GruposViewSet(viewsets.ModelViewSet):
-    serializer_class = GruposSerializer
-    queryset = Grupo.objects.all()
-    filterset_class = GrupoFilter
 
