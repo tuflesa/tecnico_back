1b9b9d5581b0cca749d59708304405b352f8d38c
tuflesa <tecnico@tuflesa.es>
2023-09-27 10:32:22 +0200

    actualizar urls.py

--- rodillos/urls.py
+++ rodillos/urls.py
@@ -17,10 +17,6 @@ router.register('seccion', SeccionViewSet)
 #router.register('lista_rodillos', RodilloViewSet)
 router.register('crear_plano', PlanoViewSet)
 router.register('revision_plano', RevisionViewSet)
-router.register('tipo_plano', TipoPlanoViewSet)
-router.register('seccion', SeccionViewSet)
-router.register('operacion', OperacionViewSet)
-# router.register('tipo_rodillo', Tipo_rodilloViewSet)
-# router.register('materiales', MaterialViewSet)
+
 
 urlpatterns = router.urls
