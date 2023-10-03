47479dd960c8cc922e3b96b09084c6f3fe5e015c
tuflesa <tecnico@tuflesa.es>
2023-10-03 10:03:52 +0200

    cambios en la serialización de los ejes de qs

--- qs/qs.py
+++ qs/qs.py
@@ -44,12 +44,12 @@ def get_ejes():
     w_lat_motor = get_real(from_PLC,68)
     w_inferior = get_real(from_PLC,72)
 
-    ejes = [{'op': 1, 'pos': [bd1_inf, bd1_sup]}, 
-            {'op': 2, 'pos': [bd2_inf, bd2_sup]},
-            {'op': 3, 'pos': [l_entrada_altura, l_entrada_ancho, l_entrada_superior, l_salida_altura, l_salida_ancho, l_salida_superior]},
-            {'op': 4, 'pos': [fp1_inf, fp1_sup]},
-            {'op': 5, 'pos': [fp2_inf, fp2_sup]},
-            {'op': 6, 'pos': [fp3_inf, fp3_sup]},
-            {'op': 7, 'pos': [w_lat_operador, w_lat_motor, w_inferior]}]
+    ejes = [{'op': 1, 'pos': {'INF': bd1_inf, 'SUP': bd1_sup}}, 
+            {'op': 2, 'pos': {'INF':bd2_inf, 'SUP': bd2_sup}},
+            {'op': 3, 'pos': {'IN_ALTO': l_entrada_altura, 'IN_ANCHO': l_entrada_ancho, 'IN_SUP': l_entrada_superior, 'OUT_ALTO': l_salida_altura, 'OUT_ANCHO': l_salida_ancho, 'OUT_SUP': l_salida_superior}},
+            {'op': 4, 'pos': {'INF': fp1_inf, 'SUP': fp1_sup}},
+            {'op': 5, 'pos': {'INF': fp2_inf, 'SUP': fp2_sup}},
+            {'op': 6, 'pos': {'INF': fp3_inf, 'SUP': fp3_sup}},
+            {'op': 7, 'pos': {'LAT_OP': w_lat_operador, 'LAT_MO': w_lat_motor, 'INF': w_inferior}}]
 
     return ejes
