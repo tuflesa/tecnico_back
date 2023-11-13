0418037b1b62b86dbcc600563724407ddc1cfecc
Mercedes Domene <mercedes.domene@bornay.es>
2023-11-13 09:49:05 +0100

    añadir longitud a las observaciones de los pedidos

--- repuestos/models.py
+++ repuestos/models.py
@@ -91,8 +91,8 @@ class Pedido(models.Model):
     creado_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     direccion_envio = models.ForeignKey(Direcciones, on_delete= models.SET_NULL, null= True, blank= True)
     contacto = models.ForeignKey(Contacto, on_delete=models.SET_NULL, null= True, blank=True)
-    observaciones = models.CharField(max_length=80, null=True, blank=True)
-    observaciones2 = models.CharField(max_length=80, null=True, blank=True)
+    observaciones = models.CharField(max_length=300, null=True, blank=True)
+    observaciones2 = models.CharField(max_length=300, null=True, blank=True)
     descripcion = models.CharField(max_length=80, null=True, blank=True)
 
     def save(self, *args, **kwargs):
