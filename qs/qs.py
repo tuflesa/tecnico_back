2643ec6da261f6a09d8cf7f56c6f94b8203f217c
tuflesa <tecnico@tuflesa.es>
2024-12-06 17:53:05 +0100

    Cambios en QS mtt2

--- qs/qs.py
+++ qs/qs.py
@@ -29,6 +29,14 @@ def get_ejes():
     bd2_inf = get_real(from_PLC,8)
     bd2_sup = get_real(from_PLC,12)
 
+    #IS
+    is1_alto = get_real(from_PLC, 156)
+    is1_ancho = get_real(from_PLC,160)
+    is2_alto = get_real(from_PLC, 164)
+    is2_ancho = get_real(from_PLC,168)
+    is3_alto = get_real(from_PLC, 172)
+    is3_ancho = get_real(from_PLC,176)
+
     # Lineal
     l_entrada_altura = get_real(from_PLC,16)
     l_entrada_ancho = get_real(from_PLC,20)
@@ -36,6 +44,9 @@ def get_ejes():
     l_salida_altura = get_real(from_PLC,28)
     l_salida_ancho = get_real(from_PLC,32)
     l_salida_superior = get_real(from_PLC, 36)
+    l_rod_inf_entrada = get_real(from_PLC, 144)
+    l_rod_inf_centro = get_real(from_PLC, 148)
+    l_rod_inf_salida = get_real(from_PLC, 152)
 
     # Fin pass
     fp1_inf = get_real(from_PLC,40)
@@ -55,15 +66,45 @@ def get_ejes():
     w_sup_ancho_op = get_real(from_PLC,192)
     w_sup_ancho_mot = get_real(from_PLC,196)
 
+    # Sizing
+    ss1_operador = get_real(from_PLC,76)
+    ss1_motor = get_real(from_PLC,80)
+    ss1_inferior = get_real(from_PLC,84)
+    ss1_superior = get_real(from_PLC,88)
+
+    ss2_operador = get_real(from_PLC,92)
+    ss2_motor = get_real(from_PLC,96)
+    ss2_inferior = get_real(from_PLC,100)
+    ss2_superior = get_real(from_PLC,104)
+
+    ss3_operador = get_real(from_PLC,108)
+    ss3_motor = get_real(from_PLC,112)
+    ss3_inferior = get_real(from_PLC,116)
+    ss3_superior = get_real(from_PLC,120)
+
+    ss4_operador = get_real(from_PLC,124)
+    ss4_motor = get_real(from_PLC,128)
+    ss4_inferior = get_real(from_PLC,132)
+    ss4_superior = get_real(from_PLC,136)
+
     ejes = [{'op': 1, 'pos': {'INF': bd1_inf, 'SUP': bd1_sup}}, 
             {'op': 2, 'pos': {'INF':bd2_inf, 'SUP': bd2_sup}},
-            {'op': 3, 'pos': {'IN_ALTO': l_entrada_altura, 'IN_ANCHO': l_entrada_ancho, 'IN_SUP': l_entrada_superior, 'OUT_ALTO': l_salida_altura, 'OUT_ANCHO': l_salida_ancho, 'OUT_SUP': l_salida_superior}},
-            {'op': 4, 'pos': {'INF': fp1_inf, 'SUP': fp1_sup}},
-            {'op': 5, 'pos': {'INF': fp2_inf, 'SUP': fp2_sup}},
-            {'op': 6, 'pos': {'INF': fp3_inf, 'SUP': fp3_sup}},
-            {'op': 7, 'pos': {'LAT_OP': w_lat_operador, 'LAT_MO': w_lat_motor, 'INF': w_inferior, 
-                              'CAB': w_cabezal, 'SUP_ALTO_OP': w_sup_alto_op, 'SUP_ALTO_MOT': w_sup_alto_mot,
-                              'SUP_ANCHO_OP': w_sup_ancho_op, 'SUP_ANCHO_MOT': w_sup_ancho_mot}}]
+            {'op': 3, 'pos': {'ALTO_S1':is1_alto, 'ANCHO_S1': is1_ancho}},
+            {'op': 4, 'pos': {'IN_ALTO': l_entrada_altura, 'IN_ANCHO': l_entrada_ancho, 'IN_SUP': l_entrada_superior, 
+                              'OUT_ALTO': l_salida_altura, 'OUT_ANCHO': l_salida_ancho, 'OUT_SUP': l_salida_superior,
+                              'ROD_INF_IN': l_rod_inf_entrada, 'ROD_INF_CENTRO': l_rod_inf_centro, 'ROD_INF_SAL': l_rod_inf_salida}},
+            {'op': 5, 'pos': {'INF': fp1_inf, 'SUP': fp1_sup}},
+            {'op': 6, 'pos': {'ALTO':is2_alto, 'ANCHO': is2_ancho}},
+            {'op': 7, 'pos': {'INF': fp2_inf, 'SUP': fp2_sup}},
+            {'op': 8, 'pos': {'ALTO':is3_alto, 'ANCHO': is3_ancho}},
+            {'op': 9, 'pos': {'INF': fp3_inf, 'SUP': fp3_sup}},
+            {'op': 10, 'pos': {'LAT_OP': w_lat_operador, 'LAT_MO': w_lat_motor, 'INF_W': w_inferior, 
+                              'CAB': w_cabezal, 'SUP_V_OP': w_sup_alto_op, 'SUP_V_MO': w_sup_alto_mot,
+                              'SUP_H_OP': w_sup_ancho_op, 'SUP_H_MO': w_sup_ancho_mot}},
+            {'op': 11, 'pos': {'INF': ss1_inferior, 'SUP': ss1_superior, 'LAT_OP': ss1_operador, 'LAT_MO': ss1_motor}},                 
+            {'op': 12, 'pos': {'INF': ss2_inferior, 'SUP': ss2_superior, 'LAT_OP': ss2_operador, 'LAT_MO': ss2_motor}},
+            {'op': 13, 'pos': {'INF': ss3_inferior, 'SUP': ss3_superior, 'LAT_OP': ss3_operador, 'LAT_MO': ss3_motor}},
+            {'op': 14, 'pos': {'INF': ss4_inferior, 'SUP': ss4_superior, 'LAT_OP': ss4_operador, 'LAT_MO': ss4_motor}}]
 
     return ejes
 
