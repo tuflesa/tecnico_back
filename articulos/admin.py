e6160660b9f72bb1d325ce4a9721903e9ccf2c75
tuflesa <tecnico@tuflesa.es>
2024-12-28 18:28:59 +0100

    crear la app de articulos

---
+++ articulos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Formas, Calidad, Acabado, Norma, Articulo
+
+admin.site.register(Formas)
+admin.site.register(Calidad)
+admin.site.register(Acabado)
+admin.site.register(Norma)
+admin.site.register(Articulo)
--- qs/admin.py
+++ qs/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from qs.models import Formas, Calidad, Acabado, Norma, Articulo, Variante
+from qs.models import  Variante
 
-admin.site.register(Formas)
-admin.site.register(Calidad)
-admin.site.register(Acabado)
-admin.site.register(Norma)
-admin.site.register(Articulo)
 admin.site.register(Variante)
