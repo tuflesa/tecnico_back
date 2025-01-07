3e2d2e9afc58d19e4743519c350385f1cec1345c
tuflesa <tecnico@tuflesa.es>
2025-01-07 17:37:51 +0100

    añadir funcion que lee las posiciones actuales de los rodillos en el PLC

--- qs/urls.py
+++ qs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EjesViewSet, PCViewSet, DiametrosActPLC
+from .views import EjesViewSet, PCViewSet, DiametrosActPLC, PosicionesActPLC
 
 urlpatterns = [
     path('ejes/', EjesViewSet.as_view()),
     path('pc/', PCViewSet.as_view()),
-    path('diametros_actuales_PLC/', DiametrosActPLC.as_view())
+    path('diametros_actuales_PLC/', DiametrosActPLC.as_view()),
+    path('posiciones_actuales_PLC/', PosicionesActPLC.as_view())
 ]
