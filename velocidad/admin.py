c6ff75713415fee56c9eca74967fe2ffdef61e43
Mercedes Domene <mercedes.domene@bornay.es>
2025-11-27 10:33:10 +0100

    Velocidad: en admin para filtrar los registros por maquina

--- velocidad/admin.py
+++ velocidad/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import ZonaPerfilVelocidad, Registro
 
+class RegistroAdmin(admin.ModelAdmin):
+    #search_fields=("zona",)
+    #list_display=("zona",)
+    list_filter=("zona",)
+
 admin.site.register(ZonaPerfilVelocidad)
-admin.site.register(Registro)
+admin.site.register(Registro, RegistroAdmin)
