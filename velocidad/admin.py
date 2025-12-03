50b7b6166973424a1a17d6b274f9742d5709f6d0
paco <tecnico@tuflesa.es>
2025-12-03 13:13:41 +0100

    Velocidad: cambio en admin

--- velocidad/admin.py
+++ velocidad/admin.py
@@ -7,12 +7,9 @@ class RegistroAdmin(admin.ModelAdmin):
     list_filter=("zona",)
 
 admin.site.register(ZonaPerfilVelocidad)
-<<<<<<< HEAD
 admin.site.register(Registro)
 admin.site.register(TipoParada)
 admin.site.register(CodigoParada)
 admin.site.register(Parada)
 admin.site.register(Periodo)
-=======
-admin.site.register(Registro, RegistroAdmin)
->>>>>>> c6ff75713415fee56c9eca74967fe2ffdef61e43
+# admin.site.register(Registro, RegistroAdmin)
