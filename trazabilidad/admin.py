b04a60fb04c70a22cd774f5e814e3beea6e7858f
paco <tecnico@tuflesa.es>
2026-03-20 12:26:17 +0100

    Trazabilidad: añadir Montajes a admin

--- trazabilidad/admin.py
+++ trazabilidad/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Acumulador, Flejes, Tubos, OF, Forma
+from .models import Acumulador, Flejes, Tubos, OF, Forma, Montaje
 
 class OFAdmin(admin.ModelAdmin):
     list_filter=("zona",)
@@ -11,3 +11,4 @@ admin.site.register(Flejes)
 admin.site.register(Tubos)
 admin.site.register(OF, OFAdmin)
 admin.site.register(Forma)
+admin.site.register(Montaje)
