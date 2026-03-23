4e75aef00f6b2337e883da66abf294833cd23ecc
Mercedes Domene <mercedes.domene@bornay.es>
2026-03-23 15:05:16 +0100

    Foso: añadimos espesor

--- foso/models.py
+++ foso/models.py
@@ -106,6 +106,7 @@ class Bobina(models.Model):
     observaciones = models.TextField(blank=True, null=True)
     creada_en = models.DateTimeField(auto_now_add=True)
     actualizada_en = models.DateTimeField(auto_now=True)
+    espesor_mm = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
 
     class Meta:
         verbose_name = "Bobina"
